d73b36d35390d47ddf6e27415fc5e81f348cf7b5
mosbymc <mosbymc@users.noreply.github.com>
2014-11-04 17:10:42 -0500

    Update validator.js

--- validator.js
+++ validator.js
@@ -431,6 +431,17 @@ var validator =  function() {
                         tested = fn.call(this, elem);
                         postValidation(tested, elem, options, value, inputsArray);
                     }
+                    else {
+                        console.log("Could not find library function: " + value " for element: " + elem);
+                        for (var k = 0; k < inputsArray.length; k++) {
+                            for (var l = 0; l < rules.length; l++) {
+                                if (elem[0] === inputsArray[k].input[0] && rules[l] === inputsArray[k].rule) {
+                                    inputsArray[k].valid = null;
+                                    break;
+                                }
+                            }
+                        }
+                    }
                 });
             }
             if (customRules !== undefined) {
@@ -462,6 +473,7 @@ var validator =  function() {
                         }
                     }
                     else {  //if the provided function name cannot be found, or isn't a function, then "ignore" as a rule we need to validate against.
+                    console.log("Could not find library function: " + value " for element: " + elem);
                         for (var k = 0; k < inputsArray.length; k++) {
                             for (var l = 0; l < rules.length; l++) {
                                 if (elem[0] === inputsArray[k].input[0] && rules[l] === inputsArray[k].rule) {
